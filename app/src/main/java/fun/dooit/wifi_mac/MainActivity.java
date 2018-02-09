b00f748a88c180b78afde2daf2450f9d62d8ed11
winricyu <winricyu@gmail.com>
2018-02-09 18:25:07 +0800

    - 存取Wifi MAC

--- app/src/main/java/fun/dooit/wifi_mac/MainActivity.java
+++ app/src/main/java/fun/dooit/wifi_mac/MainActivity.java
@@ -2,12 +2,30 @@ package fun.dooit.wifi_mac;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import fun.dooit.wifi_mac.util.net.NetworkInfo;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mTextView;
+    private Button mBtnGetMac;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnGetMac = (Button) findViewById(R.id.btn_getMac);
+        mTextView = (TextView) findViewById(R.id.textView2);
+        mBtnGetMac.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String mac = NetworkInfo.getMacAddress(this);
+        mTextView.setText(mac);
     }
 }
--- app/src/main/java/fun/dooit/wifi_mac/util/net/NetworkInfo.java
+++ app/src/main/java/fun/dooit/wifi_mac/util/net/NetworkInfo.java
@@ -1,8 +1,39 @@
 package fun.dooit.wifi_mac.util.net;
 
+import android.content.Context;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+
+import fun.dooit.wifi_mac.BuildConfig;
+
 /**
  * Created by Eric on 2018/2/9.
  */
 
 public class NetworkInfo {
+
+
+    public static String getMacAddress(Context context) {
+        String mac = "";
+        WifiManager wifimsg = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+            if (wifimsg != null) {
+                if (wifimsg.getConnectionInfo() != null) {
+                    if (wifimsg.getConnectionInfo().getMacAddress() != null) {
+                        mac = wifimsg.getConnectionInfo().getMacAddress();
+                    }
+                }
+            }
+
+
+            return mac;
+
+        }
+
+
+        return mac;
+    }
+
 }
